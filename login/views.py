6e8a04955fd1fc718f4fa42d2c2ab46d1c43c3d8
divyanshu59 <greatdivyanshu59@gmail.com>
2019-08-04 03:51:36 +0530

    Some Updated Files

--- login/views.py
+++ login/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def signup(request):
-    return render(request, 'signup.html', {})
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account Created For {username}!')
+            return redirect('home')
+
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
     
 def login(request):
     return render(request, 'login.html', {})
